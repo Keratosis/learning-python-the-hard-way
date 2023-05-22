4b8e2a827aa04a94cdee5a7b9ebe2048bc59238c
braxtonshaffie <braxtonshaffie@gmail.com>
2023-05-22 19:26:48 +0300

    done with ex8

---
+++ python projects/ex8.py
@@ -0,0 +1,18 @@
+#FORMATTING OF  STRING
+
+#the curly braces act as placeholder in the string
+formatter =" {} {} {} {}"
+
+#the .format() replaces the placeholder with whatever is passed as an argument
+print(formatter.format(1,2,3,4))
+print(formatter.format("one","two","three","four"))
+print(formatter.format(True,False,True,False))
+print(formatter.format(formatter,formatter,formatter,formatter))
+print(formatter.format(
+    "Hell Paradise",
+    "is such ",
+    "a good anime",
+    "i think I'm gonna rewatch it"
+))
+
+      
