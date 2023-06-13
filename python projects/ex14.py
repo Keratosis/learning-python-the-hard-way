e104e4b3a149ad7e61ea86e2cd82216adeefbaa7
braxtonshaffie <braxtonshaffie@gmail.com>
2023-06-13 22:38:32 +0300

    updating ex14

--- python projects/ex14.py
+++ python projects/ex14.py
@@ -16,6 +16,6 @@ computer = input(prompt)
 
 print(f"""
 Alright, so you said {likes} about liking me.
-You live in {lives}.
-And you have a {computer} computer.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.
 """)
